eb8cae2e5917dd0f64038a5c559cc3027867905c
Matias Aranda <matiasaranda426@gmail.com>
2026-08-27 18:14:04 -0300

    feat: crear vistas de la aplicación

---
+++ frontend/ToolShareCBA/src/app/pages/herramientas/herramientas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-herramientas',
+  styleUrl: './herramientas.css',
+  templateUrl: './herramientas.html',
+})
+export class Herramientas {}
---
+++ frontend/ToolShareCBA/src/app/pages/usuario/gestion-prestamo/gestion-prestamo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-gestion-prestamo',
+  styleUrl: './gestion-prestamo.css',
+  templateUrl: './gestion-prestamo.html',
+})
+export class GestionPrestamo {}
